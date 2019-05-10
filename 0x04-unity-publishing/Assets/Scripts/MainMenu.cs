e80658d3268e7902dc880e4cad2506dade4d8c33
Kevin Yook <yook00627@gmail.com>
2019-05-10 14:38:15 -0700

    Change project settings for quality

---
+++ 0x04-unity-publishing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    public void PlayMaze()
+    {
+        trapMat.color = Color.red;
+        goalMat.color = Color.green;
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
---
+++ 0x04-unity-publishing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+    private int score = 0;
+    public int health = 5;
+    public Text scoreText;
+    public Text healthText;
+    public Image winLoseBG;
+    public Text winLoseText;
+    public GameObject winLose;
+    private bool isCoroutine = true;
+
+    private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            score++;
+            SetScoreText();
+            other.gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("Trap"))
+        {
+            health--;
+            SetHealthText();
+        }
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            SetWin();
+            StartCoroutine(LoadScene(3));
+        }
+    }
+
+    private void Update()
+    {
+        if (health == 0 && isCoroutine)
+        {
+            SetGameOver();
+            StartCoroutine(LoadScene(3));
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        isCoroutine = false;
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(1);
+        isCoroutine = true;
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+
+    void SetWin()
+    {
+        winLose.SetActive(true);
+        winLoseBG.color = Color.green;
+        winLoseText.text = "You win!";
+        winLoseText.color = Color.black;
+    }
+
+    void SetGameOver()
+    {
+        winLose.SetActive(true);
+        winLoseBG.color = Color.red;
+        winLoseText.text = "Game Over!";
+        winLoseText.color = Color.white;
+    }
+}
