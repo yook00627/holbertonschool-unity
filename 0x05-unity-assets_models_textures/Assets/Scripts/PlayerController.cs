590f0c17133353ba9848747c61c72643a9258f5a
Kevin Yook <yook00627@gmail.com>
2019-05-20 22:14:42 -0700

    reset when fall

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -9,16 +9,30 @@ public class PlayerController : MonoBehaviour
     public float gravity = 20.0f;
 
     private Vector3 movement = Vector3.zero;
+    private Vector3 startPos;
     private CharacterController cc;
+    private Transform t;
 
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CharacterController>();
+        t = GetComponent<Transform>();
+        startPos = t.position;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        playerControl();
+
+        if (t.position.y < -15)
+        {
+            t.position = new Vector3(startPos.x, startPos.y + 15, startPos.z);
+        }
+    }
+
+    void playerControl()
     {
         if (cc.isGrounded)
         {
@@ -29,10 +43,8 @@ public class PlayerController : MonoBehaviour
             {
                 movement.y = jumpSpeed;
             }
-        } 
-
+        }
         movement.y -= gravity * Time.deltaTime;
-
         cc.Move(movement * Time.deltaTime);
     }
 }
