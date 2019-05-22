f24801bf98c82e4b29114e288ad2e2398a94d39a
Kevin Yook <yook00627@gmail.com>
2019-05-22 11:53:55 -0700

    added timer and start script

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+
+    private float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        timerText.text = string.Format("{0:0}:{1:00}.{2:00}", timer / 60, timer % 60, timer * 100 % 100);
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerExit(Collider other)
+    {
+        player.GetComponent<Timer>().enabled = true;
+    }
+}
