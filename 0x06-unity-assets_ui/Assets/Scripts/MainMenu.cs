b4fdd6a9001ae5c008575b771d56df5ed9642165
Kevin Yook <yook00627@gmail.com>
2019-06-17 11:53:38 -0700

    Make button funcitonality scene change

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LevelSelect(int level)
+    {
+        if (level == 1)
+            SceneManager.LoadScene("Level01");
+        if (level == 2)
+            SceneManager.LoadScene("Level02");
+        if (level == 3)
+            SceneManager.LoadScene("Level03");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+}
